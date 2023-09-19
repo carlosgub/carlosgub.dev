d8cdf46fbc055a7f246f261058254be1c2ba317b
carlosgub <r2d2@R2D2s-MacBook-Pro.local>
2023-09-19 11:29:53 -0500

    change theme to light as default

--- site/src/jsMain/kotlin/carlosgub/dev/MyApp.kt
+++ site/src/jsMain/kotlin/carlosgub/dev/MyApp.kt
@@ -33,7 +33,7 @@ fun initSilk(ctx: InitSilkContext) {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     SilkApp {
-        val colorMode = ColorMode.DARK
+        val colorMode = ColorMode.LIGHT
         LaunchedEffect(colorMode) {
             localStorage.setItem(COLOR_MODE_KEY, colorMode.name)
         }

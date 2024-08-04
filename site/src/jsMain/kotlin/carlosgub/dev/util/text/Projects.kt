3f0b36f4e4ba4374c86bde6e396da057cf9dd71e
carlosgub <carlosgabrielugazburga@gmail.com>
2024-08-04 00:25:04 -0500

    Upgrade website files

--- site/src/jsMain/kotlin/carlosgub/dev/util/text/Projects.kt
+++ site/src/jsMain/kotlin/carlosgub/dev/util/text/Projects.kt
@@ -3,7 +3,7 @@ package carlosgub.dev.util.text
 import carlosgub.dev.components.models.Stack
 
 
-val piggyBankDescription = "I create the PiggyBank App is a versatile tool for managing your finances. Whether you want to track expenses," +
+val piggyBankDescription = "I created the PiggyBank App to have a versatile tool for manage your finances. Whether you want to track expenses," +
         " log incomes, or visualize your financial movements through charts, this app has got you covered. " +
         "It offers a user-friendly interface and robust functionalities to help you stay on top of your finances."
 val piggyBankStack = listOf(

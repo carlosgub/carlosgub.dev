a121f40495bb59a9a30c4e2b6d068a59e254e34c
carlosgub <carlosgabrielugazburga@gmail.com>
2024-09-21 19:01:58 -0500

    Fix smooth scroll

--- site/src/jsMain/kotlin/carlosgub/dev/MyApp.kt
+++ site/src/jsMain/kotlin/carlosgub/dev/MyApp.kt
@@ -2,6 +2,9 @@ package carlosgub.dev
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import com.varabyte.kobweb.compose.css.ScrollBehavior
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.scrollBehavior
 import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
@@ -9,14 +12,23 @@ import com.varabyte.kobweb.silk.components.layout.Surface
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
-import com.varabyte.kobweb.silk.style.layer.SilkLayer
-import com.varabyte.kobweb.silk.style.layer.add
 import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import kotlinx.browser.localStorage
+import org.jetbrains.compose.web.css.CSSMediaQuery
+import org.jetbrains.compose.web.css.StylePropertyValue
 import org.jetbrains.compose.web.css.percent
 
 private const val COLOR_MODE_KEY = "dev:colorMode"
+
+@InitSilk
+fun enableSiteWideSmoothScrolling(ctx: InitSilkContext) = ctx.stylesheet.apply {
+    registerStyle("html") {
+        cssRule(CSSMediaQuery.MediaFeature("prefers-reduced-motion", StylePropertyValue("no-preference"))) {
+            Modifier.scrollBehavior(ScrollBehavior.Smooth)
+        }
+    }
+}
 
 @App
 @Composable

4c681109d446107d60f7f264367b3d521b2a97fd
carlosgub <carlosgabrielugazburga@gmail.com>
2024-09-23 17:11:55 -0500

    Improve footer

--- site/src/jsMain/kotlin/carlosgub/dev/components/sections/Footer.kt
+++ site/src/jsMain/kotlin/carlosgub/dev/components/sections/Footer.kt
@@ -1,29 +1,45 @@
 package carlosgub.dev.components.sections
 
 import androidx.compose.runtime.Composable
-import carlosgub.dev.components.styles.DevelopedByLink
 import carlosgub.dev.components.styles.FooterStyle
+import carlosgub.dev.components.styles.components.H6Style
+import carlosgub.dev.components.styles.components.HRStyle
 import carlosgub.dev.components.theme.WebColors
+import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.icons.fa.*
-import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.layout.HorizontalDivider
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.style.toModifier
+import org.jetbrains.compose.web.css.AlignItems
+import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.A
+import org.jetbrains.compose.web.dom.Br
+import org.jetbrains.compose.web.dom.H6
+import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun Footer(breakpoint: Breakpoint) {
-    if (breakpoint >= Breakpoint.MD) {
-        FooterHorizontal()
-    } else {
-        FooterVertical()
+    Box(
+        modifier = Modifier
+            .background(Color.floralwhite)
+            .fillMaxWidth()
+    ) {
+        HorizontalDivider(HRStyle.toModifier())
+        if (breakpoint >= Breakpoint.MD) {
+            FooterHorizontal()
+        } else {
+            FooterVertical()
+        }
     }
 }
 
@@ -31,15 +47,19 @@ fun Footer(breakpoint: Breakpoint) {
 private fun FooterHorizontal() {
     val iconSize = IconSize.XXL
     Row(
-        FooterStyle.toModifier()
+        modifier = FooterStyle
+            .toModifier()
             .padding(
                 leftRight = 80.px,
                 topBottom = 50.px
             ),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(modifier = Modifier.weight(1)) {
-            DevelopedByFooter()
+        Row(modifier = Modifier.weight(1).alignItems(AlignItems.Center)) {
+            FooterBrandImage()
+            DevelopedByFooter(
+                modifier = Modifier.fillMaxHeight().margin(left = 8.px)
+            )
         }
         Row(
             modifier = Modifier.weight(1),
@@ -67,7 +87,12 @@ private fun FooterVertical() {
             ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        DevelopedByFooter()
+        FooterBrandImage()
+        DevelopedByFooter(
+            modifier = Modifier
+                .margin(top = 8.px)
+                .textAlign(TextAlign.Center)
+        )
         Row(
             modifier = Modifier.fillMaxWidth()
                 .margin(top = 25.px, bottom = 30.px),
@@ -83,12 +108,16 @@ private fun FooterVertical() {
 }
 
 @Composable
-private fun DevelopedByFooter() {
-    Link(
-        path = "https://github.com/carlosgub/carlosgub.dev",
-        text = "Developed by Carlos Ugaz 2024",
-        modifier = DevelopedByLink.toModifier()
-    )
+private fun DevelopedByFooter(
+    modifier: Modifier = Modifier,
+) {
+    H6(
+        attrs = H6Style.toModifier().then(modifier).toAttrs()
+    ) {
+        Text("Carlos Ugaz")
+        Br()
+        Text("Senior Software Engineer")
+    }
 }
 
 @Composable
@@ -164,4 +193,13 @@ private fun MediumIcon(iconSize: IconSize) {
             modifier = Modifier.color(WebColors.Blue)
         )
     }
+}
+
+@Composable
+private fun FooterBrandImage() {
+    Image(
+        src = "logo.webp",
+        alt = "Brand Image",
+        modifier = Modifier.size(64.px)
+    )
 }
